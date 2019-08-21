c8ace5030a7ad65aaddb2227ec8fb3b8bdb86900
Emran <xmanemran@gmail.com>
2019-08-21 13:20:36 +0600

    Add system color

---
+++ src/uielements/color.js
@@ -0,0 +1,11 @@
+export default {
+  darkPrimary: '#2699FB',
+  primary: '#7FC4FD',
+  midPrimary: '#BCE0FD',
+  lightPrimary: '#F1F9FF',
+  white: '#FFFFFF',
+  primaryText: '#FFFFFF',
+  body: '#7F7F7F',
+  secondaryBody: '#CCCCCC',
+  placeHolder: '#B4B4B4',
+};
